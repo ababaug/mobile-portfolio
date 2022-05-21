2ac6374ae1fd4245ab0fc94ed130f538b3459e75
Augustine Abah <austync1988@gmail.com>
2022-05-21 14:25:56 +0300

    added modal for pop-up

---
+++ pop-up-window.js
@@ -0,0 +1,36 @@
+// var work = document.querySelector('.project-section');
+// work.innerHTML = `
+//     <div class="project-holder-1"></div>
+//     <h2>Multi-Post Stories</h2>
+//     <p>A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.
+//     </p>
+//     <ul class="project-list">
+//         <li><a href="#">CSS</a></li>
+//         <li><a href="#">HTML</a></li>
+//         <li><a href="#">Bootstrap</a></li>
+//         <li><a href="#">Ruby</a></li>
+//     </ul>
+//     <button type="submit" class="project-button see-project">See Project</button>
+// `;
+//  var popUp = document.querySelector('.see-project');
+//  popUp.addEventListener('click',() => {
+    
+//  });
+
+
+
+// p
+//     <div class="project-holder-1"></div>
+//     <div class="project-section">
+//         <h2>Multi-Post Stories</h2>
+//         <p>A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.
+//         </p>
+        
+//         <ul class="project-list">
+//             <li><a href="#">CSS</a></li>
+//             <li><a href="#">HTML</a></li>
+//             <li><a href="#">Bootstrap</a></li>
+//             <li><a href="#">Ruby</a></li>
+//         </ul>
+//         <button type="submit" class="project-button see-project">See Project</button>
+//     </div>
